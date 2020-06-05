e437ba886b2a3b2b90ca06e41bcaa267d83cffaa
Andrew Brought <andrewbrought@gmail.com>
2020-06-05 14:40:39 -0500

    setup todo list, next up, react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Headers from "./components/layout/headers";
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 
 import './App.css';
 
@@ -7,27 +10,58 @@ class App extends Component {
     state = {
         todos: [
             {
-            id: 1,
+            id: uuid.v4(),
             title: 'Take out the trash',
             completed: false
             },
             {
-            id: 2,
+            id: uuid.v4(),
             title: 'Dinner with wife',
             completed: false
             },
             {
-            id: 3,
+            id: uuid.v4(),
             title: 'Meeting with boss',
             completed: false
             }
         ]
     }
+
+    // Toggle Complete
+    markComplete = (id) => {
+        this.setState({ todos: this.state.todos.map(todo => {
+            if(todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo;
+            }) });
+    }
+
+  //   Delete Todo
+    delTodo = (id) => {
+        this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }); //We're looping through and returning a todo that doesn't match the id that's being passed in, thus creating the delete functionality
+    }
+
+  //  Add Todo
+    addTodo = (title) => {
+        // console.log(title)
+        const newTodo = {
+            id: uuid.v4(),
+            title,
+            completed: false
+        }
+        this.setState({ todos: [...this.state.todos, newTodo] })
+    }
+
   render() {
         // console.log(this.state.todos);
     return (
         <div className="App">
-          <Todos todos={this.state.todos} />
+            <div className="container">
+                <Headers />
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+            </div>
         </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,10 +3,30 @@ import PropTypes from 'prop-types';
 import Todos from "./Todos";
 
 export class TodoItem extends Component {
+    getStyle = () => {
+       return {
+           background: '#f4f4f4',
+           padding: '10px',
+           borderBottom: '1px #ccc dotted',
+           textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+       }
+    }
+
+    // markComplete = (e) => {
+    //     console.log(this.props)  //before we started "component drilling"
+    // }
+
+
     render() {
+        // This is called destructuring - where we pull the variables out of the todo and the props
+        const { id, title } = this.props.todo;
         return (
-            <div>
-             <p>{ this.props.todo.title }</p>
+            <div style={this.getStyle()}>
+                 <p>
+                     <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
+                     { title }
+                     <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>X</button>
+                 </p>
             </div>
         )
     }
@@ -15,6 +35,16 @@ export class TodoItem extends Component {
 // PropTypes
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 10px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
 }
 
 export default TodoItem
